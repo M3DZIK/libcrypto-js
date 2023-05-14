505fa3b276f95bd22f14b0730a825e48002adeb4
M3DZIK <me@medzik.dev>
2023-05-14 21:12:32 +0200

    Fix argon2 in browser enviroment (like vite)

--- src/argon2.test.ts
+++ src/argon2.test.ts
@@ -2,40 +2,58 @@ import { argon2, salt } from '.';
 
 describe('argon2', () => {
     test('hash', async () => {
-        const hash = await argon2.hash({
+        // argon2i
+        const hashI = await argon2.I({
             hashLength: 256 / 8, // 256 bits
             iterations: 1,
-            memory: 65536,
+            memorySize: 65536,
             parallelism: 1,
-            type: argon2.Argon2id,
             password: 'password',
             salt: salt.generate(16),
         });
+        expect(hashI).toEqual(expect.any(String));
 
-        expect(hash.encoded).toEqual(expect.any(String));
-        expect(hash.hash).toEqual(expect.any(String));
+        // argon2d
+        const hashD = await argon2.D({
+            hashLength: 256 / 8, // 256 bits
+            iterations: 1,
+            memorySize: 65536,
+            parallelism: 1,
+            password: 'password',
+            salt: salt.generate(16),
+        });
+        expect(hashD).toEqual(expect.any(String));
+
+        // argon2id
+        const hashID = await argon2.ID({
+            hashLength: 256 / 8, // 256 bits
+            iterations: 1,
+            memorySize: 65536,
+            parallelism: 1,
+            password: 'password',
+            salt: salt.generate(16),
+        });
+        expect(hashID).toEqual(expect.any(String));
     })
 
     test('check reproducibility', async () => {
         const inputSalt = salt.generate(16);
         const password = 'password';
 
-        const hash1 = await argon2.hash({
+        const hash1 = await argon2.ID({
             hashLength: 256 / 8, // 256 bits
             iterations: 1,
-            memory: 65536,
+            memorySize: 65536,
             parallelism: 1,
-            type: argon2.Argon2id,
             password,
             salt: inputSalt,
         });
 
-        const hash2 = await argon2.hash({
+        const hash2 = await argon2.ID({
             hashLength: 256 / 8, // 256 bits
             iterations: 1,
-            memory: 65536,
+            memorySize: 65536,
             parallelism: 1,
-            type: argon2.Argon2id,
             password,
             salt: inputSalt,
         });
@@ -46,6 +64,11 @@ describe('argon2', () => {
     test('valid hash', async () => {
         const hash = "$argon2id$v=19$m=15360,t=2,p=1$bWVkemlrQGR1Y2suY29t$n7wCfzdczbjclMnpvw+t/4D+mCcCFUU+hm6Z85k81PQ";
 
-        await argon2.verify("medzik@duck.com", hash)
+        const valid = await argon2.verify({
+            hash,
+            password: 'medzik@duck.com',
+        })
+
+        expect(valid).toBe(true);
     })
 })
