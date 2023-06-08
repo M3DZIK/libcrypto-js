d6f8b0d849fc12e548f3bfa4e3a89b802fb89256
M3DZIK <me@medzik.dev>
2023-06-08 14:45:25 +0200

    Add AES-GCM

--- src/aes.test.ts
+++ src/aes.test.ts
@@ -1,7 +1,7 @@
 import { aes, pbkdf2 } from "."
 
 describe("AES", () => {
-    test("Encrypt and Decrypt", async () => {
+    test("CBC Encrypt and Decrypt", async () => {
         const clearText = "hello world"
         const salt = "salt"
         const secretKey = await pbkdf2.hash256("secret passphrase", salt, 1000)
@@ -13,12 +13,34 @@ describe("AES", () => {
         expect(decryptedText).toBe(clearText)
     })
 
-    test("Decrypt", async () => {
+    test("CBC Decrypt", async () => {
         const salt = "salt"
         const secretKey = await pbkdf2.hash256("secret passphrase", salt, 1000)
         const cipherText = "ceb5156163e045c920cea4748ae302c7e210b4d521925bc342c71145aef3952d"
 
         const decryptedText = aes.decryptAesCbc(secretKey, cipherText)
+
+        expect(decryptedText).toBe("hello world")
+    })
+
+    test("GCM Encrypt and Decrypt", async () => {
+        const clearText = "hello world"
+        const salt = "salt"
+        const secretKey = await pbkdf2.hash256("secret passphrase", salt, 1000)
+
+        const cipherText = aes.encryptAesGcm(secretKey, clearText)
+
+        const decryptedText = aes.decryptAesGcm(secretKey, cipherText)
+
+        expect(decryptedText).toBe(clearText)
+    })
+
+    test("GCM Decrypt", async () => {
+        const salt = "salt"
+        const secretKey = await pbkdf2.hash256("secret passphrase", salt, 1000)
+        const cipherText = "0996c65a72a60e748415dc6d32da1d4dcb65f41c71d4bec9554424218839b5d4b9d9195e5eea9d"
+
+        const decryptedText = aes.decryptAesGcm(secretKey, cipherText)
 
         expect(decryptedText).toBe("hello world")
     })
--- src/aes.ts
+++ src/aes.ts
@@ -51,3 +51,58 @@ export function decryptAesCbc(secretKey: string, cipherText: string): string {
 
     return clearText
 }
+
+/**
+ * Encrypts the clear text using AES-256-GCM algorithm
+ * @param secretKey The secret key to encrypt the data.
+ * @param clearText The data to encrypt.
+ * @returns The encrypted data as a hex string.
+ */
+export function encryptAesGcm(secretKey: string, clearText: string): string {
+    // decode the secret key from a hex string to a buffer
+    const key = Buffer.from(secretKey, 'hex')
+    // generate a random initialization vector
+    const iv = salt(12)
+
+    // create a cipher using the secret key and the initialization vector
+    const cipher = createCipheriv('aes-256-gcm', key, iv)
+    // update the cipher with the clear text
+    let cipherText = cipher.update(clearText, 'utf8', 'hex')
+    // finalize the cipher
+    cipherText += cipher.final('hex')
+
+    // add the initialization vector and the auth tag to the cipher text
+    cipherText = Buffer.from(iv).toString('hex') + cipherText + cipher.getAuthTag().toString('hex')
+
+    return cipherText
+}
+
+/**
+ * Decrypts the cipher text using AES-256-GCM algorithm
+ * @param secretKey The secret key to decrypt the data.
+ * @param cipherText The data to decrypt.
+ * @returns The decrypted data as a string.
+ */
+export function decryptAesGcm(secretKey: string, cipherText: string): string {
+    // decode the secret key from a hex string to a buffer
+    const key = Buffer.from(secretKey, 'hex')
+    // get the initialization vector from the cipher text
+    const iv = Buffer.from(cipherText.substring(0, 24), 'hex')
+    // remove the initialization vector from the cipher text
+    cipherText = cipherText.substring(24)
+    // get the auth tag from the cipher text
+    const authTag = Buffer.from(cipherText.substring(cipherText.length - 32), 'hex')
+    // remove the auth tag from the cipher text
+    cipherText = cipherText.substring(0, cipherText.length - 32)
+
+    // create decipher using the secret key and the initialization vector
+    const cipher = createDecipheriv('aes-256-gcm', key, iv)
+    cipher.setAuthTag(authTag)
+
+    // update decipher with the cipher text
+    let clearText = cipher.update(cipherText, 'hex', 'utf8');
+    // finalize decipher
+    clearText += cipher.final()
+
+    return clearText
+}
